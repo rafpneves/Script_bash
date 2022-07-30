0472efcbf9010ab88664cb1b7a32a52d2c03e5f5
ascoid <rafahove2@gmail.com>
2022-07-30 10:19:58 -0400

    30/07/2022

--- Git_Up.sh
+++ Git_Up.sh
@@ -10,9 +10,31 @@ echo "    \:\|_  _\/   _\::\ \__    \::\ \    _______  \:\ \:\ \  \: ___\/ "
 echo "     \:\_\ \ \  /__\::\__/\    \::\ \  /______/\  \:\_\:\ \  \ \ \   "
 echo "      \_____\/  \________\/     \__\/  \__::::\/   \_____\/   \_\/   "
 echo "======================================================================="
-echo "|                           Qual comentario do commit?                |"
+echo "|                              Qual comando?                          |"
 echo "======================================================================="
-read commit
+echo "======================================================================="
+echo "|                                                                     |"
+echo "|                         [01] Permissão a pasta                      |"
+echo "|                         [02] Apagar a linha de tempo .git           |"
+echo "|                         [03] Iniciar uma linha de tempo             |"
+echo "|                         [04] Atualizar projeto add .                |"
+echo "|                                                                     |"
+echo "======================================================================="
+read dado
+
+if [ "$dado" -eq 1 ];
+then
+	Permissao
+elif [ "$dado" -eq 2 ];
+then
+    apaga_git
+elif [ "$dado" -eq 3 ];
+then
+    inicio_git
+else
+    atualiza_git
+fi
+
 }
 
 function /titulo() {
@@ -21,40 +43,110 @@ echo "|                           Concuido o envio do Git_UP                |"
 echo "======================================================================="
 }
 
+function Permissao() {
+echo "======================================================================="
+echo "|                               Pasta que vc esta!                    |"
+echo "======================================================================="
+echo ""
+pwd
+echo ""
+echo "======================================================================="
+echo "|                      Qual pasta vc quer dar a permissão?            |"
+echo "======================================================================="
+read commit
+echo ""
+sudo chown -R -v "ascoid" "$commit"
+echo ""
+/titulo
+}
+
 function apaga_git() {
+echo ""
 # Apaga o .git para iniciar um novo
 sudo rm -r .git
+echo ""
+/titulo
 }
 
 function inicio_git() {
-titulo
-
 # Inicio de um nova linha de tempo
 git init
 git add .
+echo "======================================================================="
+echo "|                           Qual comentario do commit?                |"
+echo "======================================================================="
+read commit
+echo ""
+echo "$commit"
 git commit -m "$commit"
 git branch -M main
-git remote add origin git@github.com:Ascoid/Script_bash.git
+echo ""
+echo "======================================================================="
+echo "|            Qual origin? exp.git@github.com:Ascoid/Doc.git           |"
+echo "======================================================================="
+read origin
+echo ""
+git remote add origin "$origin"
 git push -u origin main
-
+echo ""
 /titulo
 }
 
 function atualiza_git() {
-titulo
-
 # Atualizaçao simples de envio/atualizaçao
 git add .
+echo "======================================================================="
+echo "|                           Qual comentario do commit?                |"
+echo "======================================================================="
+read commit
+echo ""
 git commit -m "$commit"
-git push -u origin main
+echo ""
+echo "======================================================================="
+echo "|                               Pasta que vc esta!                    |"
+echo "======================================================================="
+echo ""
+pwd
+echo ""
 
-/titulo
+echo "======================================================================="
+echo "|                           Origin que esta informado                 |"
+echo "======================================================================="
+echo ""
+git remote -v 
+echo ""
+
+echo "======================================================================="
+echo "|                        Qual mudar a Origin? [s] ou [n]              |"
+echo "======================================================================="
+read res
+
+if [ $res = "s" ];
+then
+    echo "======================================================================="
+    echo "|            Qual origin? exp.git@github.com:Ascoid/Doc.git           |"
+    echo "======================================================================="
+    read origin
+    echo ""
+    git remote add origin "$origin"
+    git push -u origin main
+    echo ""
+elif [ $res = "n" ];
+then
+    echo ""
+	git push -u origin main
+    echo ""
+    /titulo
+else
+    echo "======================================================================="
+    echo "|                          Erro na resposta!!                         |"
+    echo "======================================================================="
+fi
+
 }
 
 ################# Chamado de Funcrtion ##################
+titulo
 
-#atualiza_git
-#inicio_git
-atualiza_git
-#apaga_git
+
 
--- Rede/Script_Rede.sh
+++ Rede/Script_Rede.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
-echo "==========================================[ Reiniciando rede ]============================================="
-sudo service network-manager restart
+echo "==========================================[ Reiniciando rede ]================================"
+
+#sudo service network-manager restart
+
+echo "[-] Desativando Rede WIFI..."
+sudo ifconfig enp0s3 down
+
+echo "[+] Ativando Rede WIFI"
+sudo ifconfig enp0s3 up
+
 echo ""
-echo "==============================================[ Concluido ]================================================"
+echo "==============================================[ Concluido ]==================================="
 
